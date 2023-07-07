76aaf146881012ed6dffd11616db8eb54f8a1e43
Ahmed-Shaaban-Elbehary <55551677+Ahmed-Shaaban-Elbehary@users.noreply.github.com>
2023-07-07 16:31:00 +0300

    #

---
+++ SupplyChain.App/wwwroot/js/product.js
@@ -0,0 +1,14 @@
+﻿/******************************
+******* EVENT LISTENERS *******
+*******************************/
+
+const fileInput = document.getElementById('image-input');
+const fileLabel = fileInput.nextElementSibling;
+
+fileInput.addEventListener('change', (event) => {
+    const fileName = event.target.value.split('\\').pop();
+    if (fileName != "")
+        fileLabel.innerHTML = fileName;
+    else
+        fileLabel.innerHTML = "Choose Image";
+});
