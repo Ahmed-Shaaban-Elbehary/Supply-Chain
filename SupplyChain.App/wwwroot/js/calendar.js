ba16e556df7fb07236f3bb28e4d016b8417edd21
Ahmed-Shaaban-Elbehary <55551677+Ahmed-Shaaban-Elbehary@users.noreply.github.com>
2023-08-31 13:09:56 +0300

    #calendar1

---
+++ SupplyChain.App/wwwroot/js/calendar.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
+
+    calendar.on('dateClick', function (info) {
+        console.log('clicked on ' + info.dateStr);
+    });
+});
+
+
