a4fae5fa52f00646e751d1cfb193614146d32847
Ahmed-Shaaban-Elbehary <55551677+Ahmed-Shaaban-Elbehary@users.noreply.github.com>
2023-10-15 14:11:28 +0300

    #add ProductQuantityRequest entity

---
+++ SupplyChain.Infrastructure/Configurations/ProductQuantityRequestConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SupplyChain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyChain.Infrastructure.Configurations
+{
+    public class ProductQuantityRequestConfiguration : IEntityTypeConfiguration<ProductQuantityRequest>
+    {
+        public void Configure(EntityTypeBuilder<ProductQuantityRequest> builder)
+        {
+            // Primary key
+            builder.HasKey(pqr => pqr.Id);
+
+            // Properties
+            builder.Property(pqr => pqr.QuantityToAdd).IsRequired();
+            builder.Property(pqr => pqr.RequestIn).IsRequired();
+            builder.Property(pqr => pqr.RequestedBy).IsRequired();
+            builder.Property(pqr => pqr.Reason).IsRequired();
+            builder.Property(pqr => pqr.Status).IsRequired();
+
+            // Configure the relationship with Event
+            builder.HasOne(pqr => pqr.AssociatedEvent)
+                .WithMany()
+                .HasForeignKey(pqr => pqr.EventId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Configure the relationship with Product
+            builder.HasOne(pqr => pqr.Product)
+                .WithMany()
+                .HasForeignKey(pqr => pqr.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ SupplyChain.Infrastructure/Migrations/20231015111005_create-product-additional-quantity-request-entity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupplyChain.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class createproductadditionalquantityrequestentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductQuantityRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuantityToAdd = table.Column<int>(type: "int", nullable: false),
+                    RequestIn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RequestedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    EventId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductQuantityRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductQuantityRequests_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductQuantityRequests_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductQuantityRequests_EventId",
+                table: "ProductQuantityRequests",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductQuantityRequests_ProductId",
+                table: "ProductQuantityRequests",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductQuantityRequests");
+        }
+    }
+}
